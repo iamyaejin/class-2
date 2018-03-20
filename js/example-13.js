adc89b0b30318fd045dc63847e6d0a24b2379a07
iamyaejin <yejin.louise@gmail.com>
2018-03-20 17:21:13 +0900

    모바일 네뮤 제어를 위한 버튼 완성

--- js/example-13.js
+++ js/example-13.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
     // console.log("ok!");
     var menubar = $('.menubar, .btn-menubar');
-    var menu= $('.menu')
+    var bar = $('.menubar');
+    var btn = $('.btn-menubar');
+    var menu= $('.menu');
+
     menubar.on('click',function(){
         menu.toggleClass('menu-act');
+        bar.toggleClass('menubar-act');
+        if(menu.hasClass('menu-act')){
+            btn.text('메뉴닫기');
+        }else{
+            btn.text('메뉴열기');
+        }
     });
 });
